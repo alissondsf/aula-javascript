88f071fb2463a2ac8c54ba238d721689296bf0c7
alissondsf <alissondasilvafalcao123@gmail.com>
2024-10-02 22:27:07 -0300

    .\README.md

---
+++ media.js
@@ -0,0 +1,13 @@
+const nota1 = 6;
+const nota1 = 4;
+const nota1 = 9;
+
+const media = (nota1 + nota2 + nota3) / 3;
+
+if (media >= 7) {
+    console.log("aluno aprovado com média", media);
+}else {
+    console.log("Aluno reprovado com média", media);
+}
+
+    
